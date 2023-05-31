6b2ffe0027d5283fc476cc3eb4db4a4a8fa23fd7
Reykez <minecraftreyz@gmail.com>
2023-05-31 15:53:54 +0200

    Make usage of base_channel_id variable.

--- cog/channel_manager.py
+++ cog/channel_manager.py
@@ -12,6 +12,9 @@ class ChannelManagerCog(commands.Cog):
 
     @commands.command(aliases=['cc'])
     async def createChannel(self, ctx, *args):
+        if not isinstance(ctx.channel, discord.TextChannel) or ctx.channel.id != base_channel_id:
+            return
+
         channel_name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
         category = discord.utils.get(ctx.guild.categories, id=channel_category_id)
         channel = await ctx.guild.create_text_channel(channel_name, category=category)
